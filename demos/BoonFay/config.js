0cc95952af5d46e951728a1877fd78655d23c134
Kai Louie-Badua <kailouiebadua@gmail.com>
2025-07-20 13:22:37 -0500

    somethign

--- demos/BoonFay/config.js
+++ demos/BoonFay/config.js
@@ -1,4 +1,4 @@
 // This file will be populated with environment variables during build
 window.FITNESS_SURVEY_CONFIG = {
-    webhookUrl: '__BOONFAY_WEBHOOK_URL__'
+    webhookUrl: ''https://n8n-latest-ccpw.onrender.com/webhook/4503dcea-4223-4221-84bf-c59dc9e5f86b''
 }; 
