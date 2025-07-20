8d8bc0e7d60c3eefc7190cf327269ec03efc909b
Kai Louie-Badua <kailouiebadua@gmail.com>
2025-07-20 13:00:56 -0500

    correct url

--- demos/FitnessSurvey/config.js
+++ demos/FitnessSurvey/config.js
@@ -1,4 +1,4 @@
 // This file will be populated with environment variables during build
 window.FITNESS_SURVEY_CONFIG = {
-    webhookUrl: 'https://sweepdemo-workflow.onrender.com/webhook/18d635cc-dabb-430e-877b-b2bc9158f8bd'
+    webhookUrl: 'https://n8n-latest-ccpw.onrender.com/webhook/4503dcea-4223-4221-84bf-c59dc9e5f86b'
 }; 
